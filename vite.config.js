745b0cf9d3c07e08b6e9c74f0c8daed4b3b8804f
jiahaoli558 <lijiahao369@gmail.com>
2025-06-12 19:40:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -24,11 +24,11 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
+    port: 8080,
     host: true
   },
   preview: {
-    port: 3000,
+    port: 8080,
     host: true
   }
 })
