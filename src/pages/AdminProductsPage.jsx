b3c6db7b1b3730c95ead334b5fbab6f29f56bca0
Jiahao Li <jiahaoli@MacBook-Pro.local>
2025-06-21 16:08:06 +0800

    querrys

--- src/pages/AdminProductsPage.jsx
+++ src/pages/AdminProductsPage.jsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Badge } from '@/components/ui/badge'
-import API_BASE_URL from '../config/api';
+import API_BASE_URL from '../config/api'
 
 import { 
   Package, 
